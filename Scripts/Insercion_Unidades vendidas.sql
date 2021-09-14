b6102a94094e28544f76aaf9e717bf4459e0ed4b
AndresRojas22 <andy_chapa01@hotmail.com>
2021-09-14 11:56:55 -0500

    Scripts Insercion_Unidades vendidas

---
+++ Scripts/Insercion_Unidades vendidas.sql
@@ -0,0 +1,4 @@
+insert into unidades_vendidas values('VWCAR2020GOLST', 'GOL 2020');
+insert into unidades_vendidas values('VWCAR2020POLOAUT', 'POLO 2020');
+insert into unidades_vendidas values('VWCAR2020VENTOST', 'VENTO 2020');
+insert into unidades_vendidas values('VWCAR2020JETTAGLI', 'JETTA 2020');
